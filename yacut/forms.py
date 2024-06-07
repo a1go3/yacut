8393048b3433c64ed91dca04313e8d1bfbc44f07
Roman Tatarenkov <flexomj@gmail.com>
2024-06-07 22:23:19 +0300

    upd!

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,16 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, URLField
-from wtforms.validators import DataRequired, Length, Optional, URL
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional, URL, Regexp
+
 
 class URLForm(FlaskForm):
     original_link = URLField(
         'Длинная ссылка',
-        validators=[DataRequired(message='Обязательное поле'), URL(require_tld=True, message='Введите корректный url'),
+        validators=[DataRequired(message='Обязательное поле'),
+                    URL(require_tld=True, message='Введите корректный url'),
                     Length(1, 128)]
     )
     custom_id = StringField(
         'Ваш вариант короткой ссылки',
-        validators=[DataRequired(message='Обязательное поле'),
-                    Length(6)]
+        validators=[Optional(),
+                    Length(min=6,
+                           message='Предложенный вариант ссылки слишком короткий. '
+                                   'Введите минимум 6 символов.'),
+                    Regexp(r'[A-Za-z0-9]',
+                           message='Ссылка может состоять только из латинских букв и цифр!'),
+                    ]
     )
     submit = SubmitField('Создать')
--- yacut/views.py
+++ yacut/views.py
@@ -1,4 +1,5 @@
-from random import randrange
+import string
+from random import choices
 
 from flask import abort, flash, redirect, render_template, url_for
 
@@ -9,20 +10,36 @@ from .forms import URLForm
 
 
 @app.route('/', methods=['GET', 'POST'])
-def get_short_url():
+def index():
     form = URLForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit() and form.custom_id.data == '':
         url = URLMap(
             original=form.original_link.data,
-            short=form.custom_id.data,
+            short=get_short_url()
         )
         db.session.add(url)
         db.session.commit()
-        return render_template('url_cut.html', form=form, url=url)
-    return render_template('url_cut.html', form=form)
+        return render_template('index.html', form=form, url=url)
+    if form.validate_on_submit():
+        short = form.custom_id.data
+        if URLMap.query.filter_by(short=short).first() is not None:
+            flash('Предложенный вариант короткой ссылки уже существует')
+            return render_template('index.html', form=form)
+        url = URLMap(
+            original=form.original_link.data,
+            short=short,
+        )
+        db.session.add(url)
+        db.session.commit()
+        return render_template('index.html', form=form, url=url)
+    return render_template('index.html', form=form)
 
 
 @app.route('/<string:short>')
 def get_original_url(short):
     url = URLMap.query.filter_by(short=short).first_or_404()
     return redirect(url.original)
+
+
+def get_short_url():
+    return ''.join(choices(string.digits + string.ascii_letters, k=6))
