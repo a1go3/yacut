7d8a4704bfe14dcbb1e1b364dd20565e0cda60ba
Roman Tatarenkov <flexomj@gmail.com>
2024-06-08 22:21:56 +0300

    upd!

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -12,16 +12,22 @@ from .views import get_unique_short_id
 def get_full_url(short):
     url = URLMap.query.filter_by(short=short).first()
     if url is None:
-        raise InvalidAPIUsage('Указанный id не найден', 404)
-    return jsonify({'url': url.to_dict()['url']}), 200
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
+    return jsonify({'url': url.to_dict()['url']}), HTTPStatus.OK
 
 
 @app.route('/api/id/', methods=['POST'])
 def get_short_url():
-
-
+    try:
+        request.get_json()
+    except Exception:
+        raise InvalidAPIUsage('Отсутствует тело запроса')
 
     data = request.get_json()
+
+    if data is None:
+        raise InvalidAPIUsage('Отсутствует тело запроса')
+
     if 'custom_id' not in data or data['custom_id'] == '':
         data['custom_id'] = get_unique_short_id()
 
@@ -29,13 +35,11 @@ def get_short_url():
         raise InvalidAPIUsage('"url" является обязательным полем!')
 
     if URLMap.query.filter_by(short=data['custom_id']).first() is not None:
-        raise InvalidAPIUsage('Предложенный вариант короткой ссылки уже существует.')
-
-    if not re.match(r'[A-Za-z0-9_]+$', data['custom_id']):
         raise InvalidAPIUsage(
-            'Указано недопустимое имя для короткой ссылки')
+            'Предложенный вариант короткой ссылки уже существует.')
 
-    if len(data['custom_id']) > 16:
+    if not re.match(r'[A-Za-z0-9_]+$', data['custom_id']) or len(
+            data['custom_id']) > 16:
         raise InvalidAPIUsage(
             'Указано недопустимое имя для короткой ссылки')
 
