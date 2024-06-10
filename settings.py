f5e2be24ab16584ea1b7e86d9711ee2559e71388
Roman <flexomj@gmail.com>
2024-06-10 16:36:16 +0300

    update

--- settings.py
+++ settings.py
@@ -8,5 +8,6 @@ MAX_LENGTH_SHORT_URL = 16
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
+    STATIC_URL_PATH = ''
     STATIC_FOLDER = '../html/'
     TEMPLATE_FOLDER = '../html/templates'
--- yacut/__init__.py
+++ yacut/__init__.py
@@ -5,6 +5,7 @@ from settings import Config
 
 app = Flask(
     __name__,
+    static_url_path=Config.STATIC_URL_PATH,
     static_folder=Config.STATIC_FOLDER,
     template_folder=Config.TEMPLATE_FOLDER
 )
